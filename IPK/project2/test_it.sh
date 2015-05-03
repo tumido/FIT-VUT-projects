cc933484b8267ef9c821499cebbdf9562ad2aeab
Tomas Coufal <coufal.tom@gmail.com>
2015-05-03 19:54:21 +0200

    Improved tests, finalization

--- IPK/project2/test_it.sh
+++ IPK/project2/test_it.sh
@@ -1,12 +1,34 @@
 #!/bin/bash
-
+2>/dev/nul
+spacer=`printf '=%.s' {1..80}`
+ok="\e[1;32mOK\e[0m"
+fail="\e[1;31mFAIL\e[0m"
 # prepare the environment
-make clean
-make
+echo 'Set up the workspace'
+echo $spacer
+echo '$ make clean'
+make -s clean
+echo '$ make'
+make -s
 
 # launch server
+echo $spacer
+echo -ne "Launch server in the default configuration: \t"
 ./ipkhttpserver &
+pid=$!
+disown $pid
+sleep 0.2
+if [[ $? -eq 0 ]]
+then
+  echo -e $ok
+else
+  echo -e $fail
+  exit $?
+fi
+echo $spacer
 
+echo 'Run the basic tests'
+echo $spacer
 for file in $(ls ./www)
 do
     ./ipkhttpclient http://localhost:8080/"$file"
@@ -14,25 +36,55 @@ do
 
     echo -n "Test on file \"www/$file\": "
 
-    if [[ -z $output ]]
+    if [[ -z $output && -n $(ls *.payload 2>/dev/null) ]]
     then
-      echo 'PASS'
+      echo -e "\t\t$ok"
     else
-      echo "FAIL, (here\'s the diff)"
-      echo '-----------------------------------------'
       echo $output
+      echo -e "\t\t$fail"
     fi
-    echo '-----------------------------------------'
     rm *.payload
-done
+  done
 
+echo $spacer
+echo 'Clean up'
+echo $spacer
 # we are done, kill the server
-kill $!
+echo "$ kill $!"
+kill $pid
+echo "$ make clean"
+make -s clean
 
 # try different port, chunksize, and delay
+echo $spacer
+echo 'Test different combinations of paramerers'
+echo $spacer
+for i in seq 1 3
+do
+  port=`shuf -i 8000-65000 -n 1`
+  delay=`shuf -i 0-1000 -n 1`
+  size=`shuf -i 1024-8192 -n 1`
 
-# try bad request
+  echo -n "Test server parameters {port: $port, delay: $delay, chunk_size: $size}: "
+  ./ipkhttpserver -c "$size" -t "$delay" -p "$port" &
+  pid=$!
+  disown $pid
+  sleep 0.2
+  ./ipkhttpclient http://localhost:"$port"/chunked.txt
+  output=$(diff *.payload www/chunked.txt)
+  if [[ -z $output && -n $(ls *.payload 2>/dev/null) ]]
+  then
+    echo -e "\t$ok"
+  else
+    echo $output
+    echo -e "\t$fail"
+  fi
+  rm *.payload
+  kill $pid
+done
+echo $spacer
+echo 'Clean up'
+echo $spacer
+echo "$ make clean"
+make -s clean
 
-# try non-existing file
-
-make clean
